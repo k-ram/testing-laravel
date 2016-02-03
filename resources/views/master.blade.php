4217828f4af1e6dc8d762d81d60c96b6cfc1454c
Kristy Ramage <kristy.raamage@gmail.com>
2016-02-03 15:26:22 +1300

    added blade template and basic pages

---
+++ resources/views/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<nav>
+		<ul>
+			<li><a href="/">home</a></li>
+			<li><a href="/about">about</a></li>
+			<li><a href="/contact">contact</a></li>
+			<li><a href="/products">products</a></li>
+		</ul>
+	</nav>
+
+	@yield('content')
+
+</body>
+</html>
